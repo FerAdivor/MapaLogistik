dcc447cd719db5572779db474c0eb674edb6ae98
Fernando SLP <fer@adivor.com>
2023-03-07 15:26:44 -0600

    Versión Febrero2023

---
+++ desktop/dinamycLanguage.js
@@ -0,0 +1,142 @@
+/*
+
+eng
+esp
+
+*/
+if (window.location.href.indexOf("#esp") > -1) {
+  window.location.hash = "#esp";
+  // LOAD SPANISH
+  $("#smartphone_continuebutton").text("Click aqui para continuar");
+  $("#smartphone_menunotice").text("Selecciona una escena");
+  $("#smartphone_closeNotice").text("Arrastre hacia arriba para cerrar");
+  // nav
+  $(".nav1-cobertura").text("Nuestra ubicación estratégica");
+  $(".nav2-mexico").text("México");
+  $(".nav3-redes").text("Red carretera y red ferroviaria");
+  $(".nav4-alrededores").text("Alrededores");
+  $(".nav5-parque").text("Parque industrial Logistik");
+  // 1
+  $(".hint_roads").text("Haz clic en cualquier parte del mapa para continuar");
+  $(".home_can").text("Canadá");
+  $(".home_usa").text("Estados Unidos");
+  $(".home_euro").text("Europa");
+  //2
+  $(".goldtriangle_cdmx").text("México");
+  $(".desc_goldentriangle").text(
+    "San Luis Potosí se encuentra en el triángulo dorado de México en el que se genera 78.1 % del PIB"
+  );
+  //3 class="marker_name-cdmx"
+  $(".marker_name-cdmx").text("CDMX");
+  $(".marker_name-altamira").text("Puerto de Altamira");
+  $(".marker_name-road57").text("Carretera 57");
+  // 3 HUD
+  $(".titulo-carretera").text("RED CARRETERA");
+  $(".titulo-trenes").text("RED FERROVIARIA");
+  //
+  $(".roadsHUD_hint").text("Seleciona una opción para conocer más");
+  $(".btn_label-85").text("Carretera 85");
+  $(".btn_label-57").text("Carretera 57");
+  $(".btn_label-49").text("Carretera 49");
+  $(".btn_label-80").text("Carretera 80");
+  $(".btn_label-37").text("Carretera 37");
+  $(".btn_label-70").text("Carretera 70");
+  //
+  $(".btn_label-lazaro").text("Lázaro Cárdenas");
+  $(".btn_label-veracruz").text("Veracrúz");
+  $(".btn_label-mexico").text("Ciudad de México");
+  $(".btn_label-tampico").text("Tampico");
+  $(".btn_label-puertos").text("Puertos");
+  //4
+  $("#hint_alrededoresSmartphone").text(
+    "Desliza el mapa de izquierda a derecha"
+  );
+  $(".surroundings-airport").text("Aeropuerto internacional");
+  $(".surroundings-uaslp").text("Universidad Autonoma de San Luis Potosí");
+  $(".surroundings-bus").text("Terminal terrestre potosina");
+  $(".surroundings-shopping").text("Centro commercial");
+  $(".surroundings-shopping_heb").text("Plaza HEB 57");
+  $(".surroundings-shopping_sendero").text("Plaza Sendero");
+  $(".surroundings-hospital_childwom").text("Hospital del niño y la mujer");
+  $(".surroundings-hospital_health").text("Hospital de la salud");
+  $(".surroundings-industrial").text("Zona industrial");
+  $(".surroundings-gm").text("Planta General Motors");
+  $(".surroundings-bmw").text("Planta BMW");
+  //5
+  $(".parkHUD_hint").text("Selecciona una opción para hacer zoom");
+  //
+  $(".zoomtxt-water").text("Planta de tratamiento de agua");
+  $(".zoomtxt-kansas").text("Vía ferroviaria Kansas City Southern");
+  $(".zoomtxt-power").text("Planta de energía eléctrica");
+  $(".zoomtxt-container").text("Terminal intermodal");
+  $(".zoomtxt-urbanizados").text("Terrenos urbanizados y disponibles");
+  $(".zoomtxt-desarrollo").text("Terreno para futuro crecimiento");
+  $(".zoomtxt-disponibles").text("Terrenos en urbanización y disponibles");
+}
+if (window.location.href.indexOf("#eng") > -1) {
+  window.location.hash = "#eng";
+  // LOAD ENGLISH
+  $("#smartphone_continuebutton").text("Click here to continue");
+  $("#smartphone_menunotice").text("Choose a scene");
+  $("#smartphone_closeNotice").text("Drag up to close");
+  // nav
+  $(".nav1-cobertura").text("Our strategic location");
+  $(".nav2-mexico").text("Mexico");
+  $(".nav3-redes").text("Roads and railways");
+  $(".nav4-alrededores").text("Surroundings");
+  $(".nav5-parque").text("Logistik Industrial Park");
+  // 1
+  $(".hint_roads").text("Click anywhere on the map to continue");
+  $(".home_can").text("Canada");
+  $(".home_usa").text("United States of America");
+  $(".home_euro").text("Europe");
+  //2
+  $(".goldtriangle_cdmx").text("Mexico");
+  $(".desc_goldentriangle").text(
+    "San Luis Potosí is located in the middle of the golden triangle of Mexico in which 78.1 % of the GDP is generated."
+  );
+  //3 class="marker_name-cdmx"
+  $(".marker_name-cdmx").text("Mexico City");
+  $(".marker_name-altamira").text("Altamira Port");
+  $(".marker_name-road57").text("Road 57");
+  // 3 HUD
+  $(".titulo-carretera").text("ROADS");
+  $(".titulo-trenes").text("RAILWAYS");
+  //
+  $(".roadsHUD_hint").text("Click an option to learn more");
+  $(".btn_label-85").text("Road 85");
+  $(".btn_label-57").text("Road 57");
+  $(".btn_label-49").text("Road 49");
+  $(".btn_label-80").text("Road 80");
+  $(".btn_label-37").text("Road 37");
+  $(".btn_label-70").text("Road 70");
+  //
+  $(".btn_label-lazaro").text("Lázaro Cárdenas");
+  $(".btn_label-veracruz").text("Veracrúz");
+  $(".btn_label-mexico").text("Mexico City");
+  $(".btn_label-tampico").text("Tampico");
+  $(".btn_label-puertos").text("Seaports");
+  //4
+  $("#hint_alrededoresSmartphone").text("Drag the map horizontally");
+  $(".surroundings-airport").text("International airport");
+  $(".surroundings-uaslp").text("San Luis Potosí university");
+  $(".surroundings-bus").text("Bus station");
+  $(".surroundings-shopping").text("Shopping center");
+  $(".surroundings-shopping_heb").text("HEB 57 Shopping center");
+  $(".surroundings-shopping_sendero").text("Sendero Shopping center");
+  $(".surroundings-hospital_childwom").text("Children and women hospital");
+  $(".surroundings-hospital_health").text("Health hospital");
+  $(".surroundings-industrial").text("Industrial Zone");
+  $(".surroundings-gm").text("General Motors industrial building");
+  $(".surroundings-bmw").text("BMW industrial building");
+  //5
+  $(".parkHUD_hint").text("Click an option to zoom in");
+  //
+  $(".zoomtxt-water").text("Water treatment plant");
+  $(".zoomtxt-kansas").text("Railway Kansas City Southern");
+  $(".zoomtxt-power").text("Power energy");
+  $(".zoomtxt-container").text("Intermodal container terminal");
+  $(".zoomtxt-urbanizados").text("Urbanized and available land");
+  $(".zoomtxt-desarrollo").text("Land for future growth");
+  $(".zoomtxt-disponibles").text("Under development");
+}
